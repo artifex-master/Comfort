f9410335f00630d836f39238d6fe0df4f4c6526f
Konstantin Pak <konstantin.pak.it@gmail.com>
2023-02-23 10:36:45 +0600

    comfort: v-1.0.0

---
+++ assets/js/slick.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.promotion-goods-slick').slick({
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 800,
+        arrows: true,
+        prevArrow: '<div class="slick-arrow-wrapper arrow-left center"><ion-icon name="arrow-back-outline"></ion-icon></div>',
+        nextArrow: '<div class="slick-arrow-wrapper arrow-right center"><ion-icon name="arrow-forward-outline"></ion-icon></div>',
+        dots: true
+    });
+
+});
